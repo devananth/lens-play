5819b1dfc1f2a09c202d8239f521bc006bd8e3cc
Devananth <devananthnkl@gmail.com>
2022-06-16 10:05:20 +0530

    add : created video context

---
+++ src/contexts/video-context.js
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { videos } from "../backend/db/videos";
+import { useAxios } from "../custom-hooks";
+
+const initialVidoeState = {
+  videos: [],
+  sortBy: "",
+};
+
+const VideoContext = createContext(initialVidoeState);
+
+const VideoProvider = ({ children }) => {
+  const { response, loader, callAPI } = useAxios();
+
+  const [videoState, setVideoState] = useState(initialVidoeState);
+
+  useEffect(() => {
+    callAPI({
+      url: "/api/videos",
+      method: "get",
+    });
+  }, []);
+
+  useEffect(() => {
+    setVideoState((prevState) => ({
+      ...prevState,
+      videos: response?.data?.videos,
+    }));
+  }, [response]);
+
+  const { Provider } = VideoContext;
+
+  return (
+    <Provider value={{ videoState, loader, setVideoState }}>
+      {children}
+    </Provider>
+  );
+};
+
+const useVideos = () => useContext(VideoContext);
+
+export { useVideos, VideoProvider };
